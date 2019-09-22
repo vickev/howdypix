9412c15eae951aeede5d8bd0a7172e50b3c534b9
Kevin Vicrey <kevin.vicrey@nexu.org>
2019-09-22 19:45:26 -0400

    Refactor in event based messages

--- apps/server/src/lib/eventEmitter.ts
+++ apps/server/src/lib/eventEmitter.ts
@@ -1,8 +1,10 @@
 import StrictEventEmitter from "strict-event-emitter-types";
 import { EventEmitter } from "events";
+import { ProcessData } from "@howdypix/shared-types";
 
 interface EventTypes {
   newFile: { root: string; path: string };
+  processedFile: ProcessData;
 }
 
 export type Events = StrictEventEmitter<EventEmitter, EventTypes>;
