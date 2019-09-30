cf0e247e38820807fc1fdbf59be61e2661c0d072
Kevin Vicrey <kevin.vicrey@nexu.org>
2019-09-29 21:02:44 -0400

    Add MUI

--- apps/webapp/pages/index.tsx
+++ apps/webapp/pages/index.tsx
@@ -1,10 +1,21 @@
 import { useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
+import { styled, withTheme } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+
 import { withApollo } from "../src/lib/with-apollo-client";
 import {
   GetAlbumQuery,
   GetAlbumQueryVariables
 } from "../src/__generated__/schema-types";
+
+const CustomPaper = styled(Paper)(props => ({
+  padding: props.theme.spacing(3, 2),
+  textAlign: "center"
+}));
 
 const GET_GREETING = gql`
   query GetAlbum {
@@ -27,7 +38,17 @@ function Hello() {
   >(GET_GREETING);
   if (loading) return <p>Loading ...</p>;
   console.log(data);
-  return <h1>Hello {data && data.getAlbum.albums[0].name}!</h1>;
+  return (
+    <Container maxWidth={"md"}>
+      <Box p={5}>
+        <CustomPaper>
+          <Typography variant="h4" component="h1" gutterBottom>
+            Hello {data && data.getAlbum.albums[0].name}!
+          </Typography>
+        </CustomPaper>
+      </Box>
+    </Container>
+  );
 }
 
 export default withApollo(Hello);
