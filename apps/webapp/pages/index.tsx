30524a3f59895cbd103e9fbfa87ddb262589e7e2
Francios Delpech <>
2019-11-02 13:02:06 -0400

    add sub-folder as button

--- apps/webapp/pages/index.tsx
+++ apps/webapp/pages/index.tsx
@@ -1,13 +1,16 @@
 import { useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 import { useTranslation } from "react-i18next";
-import { styled } from "@material-ui/core/styles";
+import { styled, makeStyles } from "@material-ui/core/styles";
+
 import Paper from "@material-ui/core/Paper";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import FolderIcon from "@material-ui/icons/Folder";
 
 import { withApollo } from "../src/lib/with-apollo-client";
 import {
@@ -17,7 +20,16 @@ import {
 
 const CustomPaper = styled(Paper)(props => ({
   padding: props.theme.spacing(3, 2),
-  textAlign: "center"
+  textAlign: "left"
+}));
+
+const useStyles = makeStyles(theme => ({
+  margin: {
+    margin: theme.spacing(1)
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1)
+  }
 }));
 
 const GET_GREETING = gql`
@@ -42,6 +54,7 @@ function Hello() {
     GetAlbumQuery,
     GetAlbumQueryVariables
   >(GET_GREETING);
+  const classes = useStyles();
 
   if (loading) return <p>Loading...</p>;
   console.log(data);
@@ -50,9 +63,24 @@ function Hello() {
     <Container maxWidth={"md"}>
       <Box p={5}>
         <CustomPaper>
-          <Typography variant="h4" component="h1" gutterBottom>
+          <Typography variant="h4" component="h1" align="center">
             Album {data && data.getAlbum.album && data.getAlbum.album.name}
           </Typography>
+          {data &&
+            data.getAlbum.albums.map(
+              album =>
+                album &&
+                album.name && (
+                  <Button
+                    size="medium"
+                    variant="outlined"
+                    className={classes.margin}
+                  >
+                    <FolderIcon className={classes.extendedIcon} />
+                    {album.name}
+                  </Button>
+                )
+            )}
           <GridList cellHeight={160} cols={3}>
             {data &&
               data.getAlbum.photos.map(
