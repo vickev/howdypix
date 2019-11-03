f9a571c4c36b8b3402e5922593adf9b646018a4d
Kevin Vicrey <kevin.vicrey@nexu.org>
2019-11-03 17:55:53 -0500

    Remove config package and rely on dotenv

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -1,8 +1,8 @@
-import config from "config";
+import config from "./config";
 import { startRabbitMq } from "./libs/startRabbitMq";
 
 async function main() {
-  await startRabbitMq(config.get("rabbitMq.url"));
+  await startRabbitMq(config.rabbitMq.url);
 }
 
 main();
