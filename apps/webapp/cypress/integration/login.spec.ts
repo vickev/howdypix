f18ef5bb607a6a6557dc8876a3b1601f9f626b8a
Kevin Vicrey <kevin.vicrey@nexu.org>
2019-11-18 08:56:54 -0500

    Remove it.only

--- apps/webapp/cypress/integration/login.spec.ts
+++ apps/webapp/cypress/integration/login.spec.ts
@@ -19,7 +19,7 @@ context("LoginBox page", () => {
     cy.findByTestId("login_form").should("contain", "error occurred");
   });
 
-  it.only("should be successful if there is the right email.", () => {
+  it("should be successful if there is the right email.", () => {
     cy.visit("/login?fixture-set=full");
     cy.findByTestId("login_email").type("success@vickev.com{enter}");
     cy.get("body").should("contain", "email has been sent to your address");
