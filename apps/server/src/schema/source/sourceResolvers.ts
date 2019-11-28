34950d71d8648f49a214df7d48bd9d258cc3caa8
Kevin Vicrey <kevin.vicrey@nexu.org>
2019-11-27 20:04:17 -0500

    Add VIEW for the sources

--- apps/server/src/schema/source/sourceResolvers.ts
+++ apps/server/src/schema/source/sourceResolvers.ts
@@ -1,10 +1,29 @@
-import { NexusGenFieldTypes } from "@howdypix/graphql-schema/schema.d";
+import {
+  NexusGenArgTypes,
+  NexusGenFieldTypes
+} from "@howdypix/graphql-schema/schema.d";
 import { UserConfigState } from "../../state";
+import { ApolloContext } from "../../types.d";
+import { Source as EntitySource } from "../../entity/Source";
 
 export const getSourcesResolver = (
   photoDirs: UserConfigState["photoDirs"]
-) => (): NexusGenFieldTypes["Query"]["getSources"] => {
+) => async (
+  root: {},
+  args: NexusGenArgTypes["Query"]["getAlbum"],
+  ctx: ApolloContext
+): Promise<NexusGenFieldTypes["Query"]["getSources"]> => {
+  const sourceRepository = ctx.connection.getRepository(EntitySource);
+  const sources: { [source: string]: EntitySource | null } = {};
+
+  (await sourceRepository.find()).forEach(source => {
+    sources[source.source] = source;
+  });
+
   return Object.keys(photoDirs).map(name => ({
-    name
+    name,
+    nbAlbums: sources[name]?.nbAlbums ?? 0,
+    nbPhotos: sources[name]?.nbPhotos ?? 0,
+    preview: sources[name]?.preview
   }));
 };
