48d7ec75acfbdc52006b02adfe6f51da4994698b
Kevin Vicrey <kevin.vicrey@nexu.org>
2019-12-07 11:53:29 -0500

    Fix lint errors and tests

--- apps/server/src/schema/source/__tests__/resolvers.spec.ts
+++ apps/server/src/schema/source/__tests__/resolvers.spec.ts
@@ -1,5 +1,7 @@
+import { Connection } from "typeorm";
 import { getSourcesResolver } from "../sourceResolvers";
 import { UserConfigState } from "../../../state";
+import { Source as EntitySource } from "../../../entity/Source";
 
 describe("currentUserResolver", () => {
   beforeEach(() => {
@@ -14,7 +16,30 @@ describe("currentUserResolver", () => {
 
   const resolver = getSourcesResolver(photoDirs);
 
+  const connection = ({
+    getRepository: () => ({
+      find: (): EntitySource[] => [
+        {
+          source: "source1",
+          nbAlbums: 1,
+          nbPhotos: 10,
+          preview: "preview1.png",
+          dir: "dir1"
+        },
+        {
+          source: "source2",
+          nbAlbums: 2,
+          nbPhotos: 20,
+          preview: "preview2.png",
+          dir: "dir2"
+        }
+      ]
+    })
+  } as unknown) as Connection;
+
   test("should return the current list of sources", async () => {
-    expect(resolver()).toMatchSnapshot();
+    expect(
+      await resolver({}, { source: "source1" }, { user: null, connection })
+    ).toMatchSnapshot();
   });
 });
--- libs/jest-config/index.js
+++ libs/jest-config/index.js
@@ -141,12 +141,10 @@ module.exports = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  testMatch: ["**/__tests__/**/*.[jt]s?(x)"]
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)"],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  testPathIgnorePatterns: ["/node_modules/", "/build/"]
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
