dd1750a2971521c961caf8cb9553f621cbf84183
Kevin Vicrey <kevin.vicrey@nexu.org>
2020-01-27 09:06:35 -0500

    Add SortButton tests

---
+++ apps/webapp/src/component/__tests__/SortButton.spec.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { SortButton } from "../SortButton";
+
+describe("SortButton", () => {
+  test("must display the buttons", async () => {
+    const { findByText } = render(
+      <SortButton value="NAME_ASC" onChange={(): void => {}} />
+    );
+
+    expect(await findByText("Sort By: Name")).toBeVisible();
+  });
+
+  test("should call the callback when the sorting changes", async () => {
+    const callback = jest.fn();
+    const { findByText, findByTestId } = render(
+      <SortButton value="NAME_ASC" onChange={callback} />
+    );
+
+    (await findByText("Sort By: Name")).click();
+    (await findByText("Date")).click();
+    expect(callback).toHaveBeenCalledWith("DATE_ASC");
+
+    (await findByTestId("orderAscDesc")).click();
+    expect(callback).toHaveBeenCalledWith("NAME_DESC");
+  });
+});
