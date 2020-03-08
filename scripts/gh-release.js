0755a8884cca77392467c73d2819dc71a1908845
Kevin Vicrey <kevin.vicrey@nexu.org>
2020-03-08 16:12:07 -0400

    ci: fix rename app-* to build apps

--- scripts/gh-release.js
+++ scripts/gh-release.js
@@ -1,4 +1,10 @@
 var ghRelease = require("gh-release");
+const version = require("../lerna.json").version;
+
+if (version.search(/alpha/)) {
+  console.log("Alpha version: we do not create a new release in Github.");
+  process.exit(1);
+}
 
 // all options have defaults and can be omitted
 var options = {
