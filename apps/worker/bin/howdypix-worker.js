efe818e44125bfd2137ba2f097b44e87207f8bf2
Kevin Vicrey <kevin.vicrey@nexu.org>
2020-03-09 22:49:16 -0400

    feat(worker): add CLI

---
+++ apps/worker/bin/howdypix-worker.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require("fs");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { execSync } = require("child_process");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { join } = require("path");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { parse } = require("yaml");
+
+require("yargs") // eslint-disable-line
+  .command(
+    "start <config_file_yaml>",
+    "start the worker",
+    yargs => {
+      yargs.positional("config_file_yaml", {
+        describe: "The configuration file to load written in YAML"
+      });
+    },
+    argv => {
+      const content = fs.readFileSync(
+        join(process.cwd(), argv.config_file_yaml)
+      );
+
+      const config = parse(content.toString());
+      execSync(
+        `NODE_CONFIG='${JSON.stringify(
+          config
+        )}' node ${__dirname}/../dist/index.js`,
+        {
+          stdio: "inherit"
+        }
+      );
+    }
+  )
+  .demandCommand().argv;
