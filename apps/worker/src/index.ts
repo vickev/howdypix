604cdb5c849ea70bc48ce350cce7cbc47eee6106
Kevin Vicrey <kevin.vicrey@nexu.org>
2020-03-22 17:25:30 -0400

    fix(worker): add retry when rabbitMQ is not ready and remove wait-on

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -1,7 +1,9 @@
+import { appInfo } from "@howdypix/utils";
 import config from "./config";
 import { startRabbitMq } from "./libs/startRabbitMq";
 
 async function main() {
+  appInfo("main")("Starting the worker.");
   await startRabbitMq(config.rabbitMq.url);
 }
 
