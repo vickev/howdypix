8097e6efa7e8f624abe6e68d0ff3ed4f0c7c0c47
Kevin Vicrey <kevin.vicrey@nexu.org>
2020-04-04 22:04:19 -0400

    refactor(worker): remove unusued code

--- apps/worker/src/libs/process.ts
+++ apps/worker/src/libs/process.ts
@@ -1,39 +1,9 @@
-import sharp from "sharp";
-import mkdirp from "mkdirp";
-import { join, parse } from "path";
+import { join } from "path";
 import { statSync } from "fs";
-import { ExifImage } from "exif";
 import { fromFile as fileTypeOf } from "file-type";
 import { ExifData, HFile, ProcessData, StatData } from "@howdypix/shared-types";
-import {
-  appDebug,
-  generateThumbnailPaths,
-  thumbnailPath,
-  hfile2path,
-} from "@howdypix/utils";
+import { appDebug, hfile2path } from "@howdypix/utils";
 import { getFileProcessor } from "../processor";
-
-export async function fetchExif(root: string, path: string): Promise<ExifData> {
-  return new Promise((resolve, reject) => {
-    // eslint-disable-next-line no-new
-    new ExifImage(join(root, path), (error, data) => {
-      if (!error) {
-        resolve({
-          ISO: data.exif.ISO,
-          aperture: data.exif.ApertureValue,
-          shutter: data.exif.ExposureTime,
-          createDate: data.exif.CreateDate
-            ? new Date(data.exif.CreateDate).getMilliseconds()
-            : undefined,
-          make: data.image.Make,
-          model: data.image.Model,
-        });
-      } else {
-        reject(error);
-      }
-    });
-  });
-}
 
 export async function fetchStat(root: string, path: string): Promise<StatData> {
   const absolutePath = join(root, path);
@@ -45,25 +15,6 @@ export async function fetchStat(root: string, path: string): Promise<StatData> {
     birthtime: stat.birthtimeMs,
     size: stat.size,
   };
-}
-
-export async function createThumbnails(
-  thumbnailsDir: string,
-  root: string,
-  hfile: HFile
-): Promise<string[]> {
-  const { dir } = parse(thumbnailPath(thumbnailsDir, hfile));
-
-  mkdirp.sync(dir);
-
-  return Promise.all(
-    generateThumbnailPaths(thumbnailsDir, hfile).map(async (data) => {
-      await sharp(join(root, hfile2path(hfile).toString()))
-        .resize(data.width)
-        .toFile(data.path);
-      return data.path;
-    })
-  );
 }
 
 export async function process(
